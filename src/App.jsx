c9d16222efee1a74c5dddb0b58052169ffc1f20b
Akshay Mahalle <akshaymahalle005@gmail.com>
2025-07-12 10:06:07 +0530

    Implimented First Part Of The Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
 import {ScrollTrigger,SplitText} from  'gsap/all';
 import gsap from 'gsap';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
 //they are now available globally in youur application
 
 function App() {
   return (
-    <div className="text-7xl flex-center h-[100vh]">Hello GSAP!</div>
+    <main>
+      <Navbar/>
+      <Hero />
+      <div className="h-dvh bg-black"/>
+    </main>
   )
 }
 
